0a74fca6523c8341002d2c53bc7c7feea4fff11c
Md. Abdul Khalek <khalek.raju.bd@gmail.com>
2024-02-29 12:20:37 +0600

    array like objects

---
+++ array-like-objects.js
@@ -0,0 +1,13 @@
+
+let numbes = [1,2,3,4,5]
+// sum(...numbes);
+// function sum(){
+//     console.log(arguments);
+//     console.log(arguments.length);
+//     console.log(arguments[0]);  
+// }
+
+// strings
+const strings = "hello";
+console.log(strings);
+console.log(strings.length);
