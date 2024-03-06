f7e75d1a4c7dc16def51563dd622c331c15bbb4e
Md. Abdul Khalek <khalek.raju.bd@gmail.com>
2024-03-06 16:32:11 +0600

    compare foreach mehtod for for of

--- loop.js
+++ loop.js
@@ -19,9 +19,17 @@ do{
 
 
 // for of loop
+// const myList = ['bangladesh','India','Nepal','Bhutan'];
 for(var country of myList){
-    console.log(country);
+    // console.log(country);
 }
+
+const numberList = [100,200,36,144,258,254];
+numberList.forEach(function(item){
+    // console.log(item);
+})
+
+
 
 // for in looop
 const person = {
@@ -30,5 +38,16 @@ const person = {
     country : 'Bangladesh'
 }
 for( let key in person){
-    console.log(person[key]);
+    // console.log(person[key]);
 }
+
+
+// object in foreach
+const car = {
+    name:'Tesla',
+    model: 2023,
+    color:'Black',
+}
+Object.values(car).forEach(values=>{
+    console.log(values);
+})
